fe93d7014420c6cf55a0c0baa0958c6ca3f796e7
kasprzyk <piotkas1@gmail.com>
2020-08-10 19:38:46 +0200

    Disabling form

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,16 @@
 const socket = io();
 
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+
 socket.on('message', (message) => {
   console.log(message);
 });
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
   e.preventDefault();
+  $messageFormButton.setAttribute('disabled', 'disabled');
 
   const message = e.target.elements.message.value;
 
