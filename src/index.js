81058d822f9cbc0cdd3498d2a23284531d41fc36
kasprzyk <piotkas1@gmail.com>
2020-08-11 20:47:19 +0200

    New utils

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const Filter = require('bad-words');
+const { generateMessage } = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -16,7 +17,7 @@ app.use(express.static(publicDirectoryPath));
 io.on('connection', (socket) => {
   console.log('New WebSocket connection');
 
-  socket.emit('message', 'Welcome!');
+  socket.emit('message', generateMessage('Welcome!'));
   socket.broadcast.emit('message', 'A new user has joined');
 
   socket.on('sendMessage', (message, callback) => {
