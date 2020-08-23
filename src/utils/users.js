41606a19b87174bfbb33ecd18a5962d5c5e9a245
kasprzyk <piotkas1@gmail.com>
2020-08-23 16:48:13 +0200

    Storing users

--- src/utils/users.js
+++ src/utils/users.js
@@ -12,4 +12,31 @@ const addUser = ({ id, username, room }) => {
   const existingUser = users.find((user) => {
     return user.room === room && user.username === username;
   });
+
+  if (existingUser) {
+    return {
+      error: 'Username is in use',
+    };
+  }
+
+  const user = { id, username, room };
+  users.push(user);
+  return { user };
 };
+
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+
+addUser({
+  id: 22,
+  username: 'Piotr',
+  room: 'Ilawa',
+});
+
+const removedUser = removeUser(22);
+console.log(removedUser);
+console.log(users);
