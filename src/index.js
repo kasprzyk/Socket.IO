b4becd5c2fde70662cc7d7283251a44e862bfe3e
kasprzyk <piotkas1@gmail.com>
2020-08-25 16:54:52 +0200

    Sending messages to rooms

--- src/index.js
+++ src/index.js
@@ -43,16 +43,18 @@ io.on('connection', (socket) => {
   });
 
   socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
     const filter = new Filter();
     if (filter.isProfane(message)) {
       return callback('Profanity is not allowed');
     }
-    io.to('Ilawa').emit('message', generateMessage(message));
+    io.to(user.room).emit('message', generateMessage(message));
     callback();
   });
 
   socket.on('sendLocation', (coords, callback) => {
-    io.emit(
+    const user = getUser(socket.id);
+    io.to(user.room).emit(
       'locationMessage',
       generateLocationMessage(
         `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
