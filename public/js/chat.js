c23daef3df10d5d938816345788dd91842367790
kasprzyk <piotkas1@gmail.com>
2020-08-26 16:44:54 +0200

    Automatic scrolling

--- public/js/chat.js
+++ public/js/chat.js
@@ -24,6 +24,12 @@ const autoscroll = () => {
   const newMessageStyles = getComputedStyle($newMessage);
   const newMessageMargin = parseInt(newMessageStyles.marginBottom);
   const newMessageHeight = $newMessage.offsetHeight;
+  const visibleHeight = $messages.offsetHeight;
+  const containerHeight = $messages.scrollHeight;
+  const scrollOffset = $messages.scrollTop + visibleHeight;
+  if (containerHeight - newMessageHeight <= scrollOffset) {
+    $messages.scrollTop = $messages.scrollHeight;
+  }
 
   console.log(newMessageStyles);
 };
